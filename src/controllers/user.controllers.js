a7a1708ef0022de16c191d88e7c7ef690710e19f
DamTchorzewski <113952006+DamTchorzewski@users.noreply.github.com>
2023-10-02 12:41:47 +0200

    Update user.controllers.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -5,20 +5,23 @@ const blacklistedTokens = new Set();
 
 const signUp = async (req, res) => {
   const { username, email, password } = req.body;
-  const user = await User.findOne({ email }).lean();
-  if (user) {
-    return res.status(409).json({
-      status: "Conflict",
-      message: "Email is already in use.",
-    });
-  }
+
   try {
+    const user = await User.findOne({ email }).lean();
+    if (user) {
+      return res.status(409).json({
+        status: "Conflict",
+        message: "Email is already in use.",
+      });
+    }
+
     const newUser = new User({
       username,
       email,
     });
     newUser.setPassword(password);
     await newUser.save();
+
     res.status(201).json({
       status: "Created",
       message: "Registration successful.",
@@ -35,22 +38,25 @@ const logIn = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+
     if (!user || !user.validPassword(password)) {
       return res.status(400).json({
-        status: "Bad request",
+        status: "Bad Request",
         message: "Incorrect login or password.",
       });
     }
+
     const payload = {
       id: user.id,
       username: user.username,
     };
     const secret = process.env.SECRET;
     const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+
     res.status(200).json({
       status: "OK",
       data: {
-        token: token,
+        token,
       },
     });
   } catch (error) {
@@ -93,19 +99,20 @@ const logOut = async (req, res) => {
 const aboutUser = async (req, res) => {
   try {
     const { username, email } = req.user;
-    await User.findOne({ email });
     const token = req.headers.authorization;
+
     if (blacklistedTokens.has(token)) {
       return res.status(401).json({
         status: "Unauthorized",
         message: "Token is blacklisted.",
       });
     }
+
     res.status(200).json({
       status: "OK",
       data: {
-        username: username,
-        email: email,
+        username,
+        email,
       },
     });
   } catch (error) {
